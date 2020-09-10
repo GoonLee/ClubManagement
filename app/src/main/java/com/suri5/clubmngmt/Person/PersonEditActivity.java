48fe700d4be328251021841ac6c5a59c5f1ac66c
howtolivelikehuman <yoicch19@gmail.com>
2020-09-10 22:10:31 +0900

    Edit image

--- app/src/main/java/com/suri5/clubmngmt/Person/PersonEditActivity.java
+++ app/src/main/java/com/suri5/clubmngmt/Person/PersonEditActivity.java
@@ -4,8 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.ImageDecoder;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -23,12 +25,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.suri5.clubmngmt.Common.Constant;
 import com.suri5.clubmngmt.Common.DatabaseHelper;
+import com.suri5.clubmngmt.Common.EdiImages;
 import com.suri5.clubmngmt.Group.Group;
 import com.suri5.clubmngmt.Group.GroupAdapter_short;
 import com.suri5.clubmngmt.R;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
+import com.yalantis.ucrop.UCrop;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import static com.suri5.clubmngmt.Common.DatabaseHelper.println;
@@ -43,6 +49,7 @@ public class PersonEditActivity extends AppCompatActivity {
     PersonDB personDB;
     Person p;
     Activity activity;
+
 
     //추가/수정 판단
     int pk = -1;
@@ -146,8 +153,11 @@ public class PersonEditActivity extends AppCompatActivity {
             @Override
             public void onClick(View view)
             {
-                picture = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.avatar_empty);
-                Glide.with(view).load(picture).error(R.drawable.avatar_empty).into(imageView);
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent, Constant.REQUEST_CODE_GET_IMAGE);
+                //picture = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.avatar_empty);
+                //Glide.with(view).load(picture).error(R.drawable.avatar_empty).into(imageView);
             }
         });
 
@@ -255,17 +265,29 @@ public class PersonEditActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode,resultCode,data);
-        /*if (requestCode == Constant.REQUEST_CODE_GET_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+        if (requestCode == Constant.REQUEST_CODE_GET_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
             Uri selectedImageUri = data.getData();
+            Uri destination = Uri.fromFile(new File(getCacheDir(), "cropped"));
             try {
-                //Todo: 빨간줄 무시 버전 체크하는 if문 넣을 예정
-                picture = ImageDecoder.decodeBitmap(ImageDecoder.createSource(getContentResolver(), selectedImageUri));
-                imageView.setImageBitmap(picture);
-            } catch (Exception e) {}
-        }*/
+                println("크롭 하기 전 주소@@@@@@@@@@@@@@@@" + selectedImageUri.toString());
+                UCrop.of(selectedImageUri, destination).start(this);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (resultCode == RESULT_OK && requestCode == UCrop.REQUEST_CROP) {
+            final Uri resultUri = UCrop.getOutput(data);
+            picture = resize_filesize(getApplicationContext(), resultUri, 107200);
+            Glide.with(this).load(picture).error(R.drawable.avatar_empty).into(imageView);
+
+        } else if (resultCode == UCrop.RESULT_ERROR) {
+            final Throwable cropError = UCrop.getError(data);
+        }
+
         if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
-
             try{
                 if (resultCode == RESULT_OK) {
                     Uri selectedImageUri = result.getUri();
