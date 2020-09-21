8f64076d9dff025f5238bdc0b4b3b1172c3c65f2
GoonLee <auteur.kor@gmail.com>
2020-09-21 22:35:53 +0900

    Group, Schedule DBManager revised

--- app/src/main/java/com/suri5/clubmngmt/Common/DBManager.java
+++ app/src/main/java/com/suri5/clubmngmt/Common/DBManager.java
@@ -1,18 +1,10 @@
 package com.suri5.clubmngmt.Common;
 
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Parcelable;
 
 import java.util.ArrayList;
 
-public abstract class DBManager<T extends Parcelable> {
-    protected SQLiteDatabase database;
-    protected DatabaseHelper dbHelper;
-
-    public DBManager(DatabaseHelper databaseHelper) {
-        this.dbHelper = databaseHelper;
-        database = dbHelper.getWritableDatabase();
-    }
+public interface DBManager<T extends Parcelable> {
 
     public abstract void insertRecord(T record);
 
